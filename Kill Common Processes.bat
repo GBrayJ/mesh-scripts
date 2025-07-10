d6491171fe308d51f3a7370eaa2cf1c899a84dca
Brayden Johnson <brayden@braydenjohnson.dev>
2025-07-10 11:56:23 -0600

    Update Kill Common Processes.bat

--- Kill Common Processes.bat
+++ Kill Common Processes.bat
@@ -1,5 +1,6 @@
 taskkill /F /IM chrome.exe /T
 taskkill /F /IM javaw.exe /T
+taskkill /F /IM Minecraft.exe /T
 taskkill /F /IM MinecraftLauncher.exe /T
 taskkill /F /IM RobloxStudioBeta.exe /T
 taskkill /F /IM RobloxPlayerBeta.exe /T
